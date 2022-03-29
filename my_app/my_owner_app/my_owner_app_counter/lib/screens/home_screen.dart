c0b967c14ca7e3d6e6e3da2fc716467cfaeeeff5
Sergio Noriega <sergio.noriega@wiedii.co>
2022-03-29 13:34:52 -0500

    Add FloatingButtons and Events and his Ubication.

--- my_app/my_owner_app/my_owner_app_counter/lib/screens/home_screen.dart
+++ my_app/my_owner_app/my_owner_app_counter/lib/screens/home_screen.dart
@@ -7,28 +7,45 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     const fontSize30 = TextStyle(fontSize: 30);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Fk U"),
-        elevation: 10.0,
-      ),
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          //mainAxis es alineacion vertical solamente
-          mainAxisAlignment: MainAxisAlignment.center,
-          //crossAxisAlignment es alineacion horizontal solamente
-          //crossAxisAlignment: CrossAxisAlignment.center,
-          //mainAxisSize: MainAxisSize.max,
-          children: const <Widget>[
-            Text("Numero de Clicks",
-              style: fontSize30
-            ),
-            Text("1",
-                style: fontSize30
-            )
-          ],
+        appBar: AppBar(
+          title: const Text("Fk U"),
+          elevation: 10.0,
         ),
-      ),
-    );
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Column(
+            //mainAxis es alineacion vertical solamente
+            mainAxisAlignment: MainAxisAlignment.center,
+            //crossAxisAlignment es alineacion horizontal solamente
+            //crossAxisAlignment: CrossAxisAlignment.center,
+            //mainAxisSize: MainAxisSize.max,
+            children: const <Widget>[
+              Text("Numero de Clicks", style: fontSize30),
+              Text("2", style: fontSize30)
+            ],
+          ),
+        ),
+        floatingActionButtonLocation:
+            FloatingActionButtonLocation.centerFloat,
+        //with icon and text
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            print("button pressed");
+          },
+          elevation: 10,
+          label: const Text('Save'),
+          icon: const Icon(Icons.save),
+          backgroundColor: Colors.green,
+        )
+        //normal icon
+
+/*        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print("button pressed");
+          },
+          tooltip: 'Approve',
+          backgroundColor: Colors.green,
+          child: const Icon(Icons.navigation, textDirection: TextDirection.rtl),
+        )*/);
   }
 }
