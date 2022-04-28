09941b216b71c835283d49c1f5b52667fcb7f794
Sergio Noriega <sergio.noriega@wiedii.co>
2022-04-28 11:40:23 -0500

    68. HomeScreen y rutas iniciales

--- flutter_components/lib/main.dart
+++ flutter_components/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_components/screens/list_view.dart';
-import 'package:flutter_components/screens/list_view_decorated.dart';
+import 'package:flutter_components/screens/screens.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,10 +11,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      home: ListViewDecorated(),
+      home: const ListViewDecorated(),
+      initialRoute: 'home',
+      routes: {
+        'home': (context) => const HomeScreen(),
+        'listView': (context) => const ListView1Screen(),
+        'listViewDecorated': (context) => const ListViewDecorated(),
+        'cardScreen': (context) => const CardScreen(),
+        'alertScreen': (context) => const AlertScreen()
+      },
     );
   }
 }
