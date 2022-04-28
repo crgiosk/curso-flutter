03cca9fd02a241afb1f89f00167f94027b50a708
Sergio Noriega <sergio.noriega@wiedii.co>
2022-04-28 08:05:05 -0500

    67. Taps sobre los ListTiles y estilo del AppBar.

--- flutter_components/lib/screens/list_view_decorated.dart
+++ flutter_components/lib/screens/list_view_decorated.dart
@@ -7,15 +7,23 @@ class ListViewDecorated extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(centerTitle: true, title: const Text("Screen ListView 1")),
+      appBar: AppBar(
+          centerTitle: true, title: const Text("Screen ListView 1"),
+        elevation: 5,
+        backgroundColor: Colors.deepOrange,
+
+      ),
         body: ListView.separated(
             itemBuilder: (context, index) => ListTile(
               //icono a la izquierda del texto
               leading: const Icon(Icons.compare_arrows),
               //icono a la derecha del texto
-              trailing: const Icon(Icons.compare_arrows),
+              trailing: const Icon(Icons.compare_arrows, color: Colors.deepOrange),
               title: Text(options[index]),
-              onTap: (){},
+              onTap: (){
+                final game = options[index];
+                print(game);
+              },
             ),
             separatorBuilder: (_ , __) => const Divider(),
             itemCount: options.length)
